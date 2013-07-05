336875669c760e7cf758c164fe9135caac0e3af8
Mark Reidy <markinnva@gmail.com>
2013-07-04 22:43:32 -0400

    oops

--- js/app_gridTest.js
+++ js/app_gridTest.js
@@ -17,63 +17,36 @@ define([
         
             startUp = function () {
            // 	console.log("Startup (app)");
-            	
-            	
+            	           	
             var grid, d, photo, togGrid, togPhoto, togmainGrid, divGrid, divPhoto; //, onH, backButton = domConstruct.create("button", {id: "backButton", innerHTML:"To Main"});
 
        		thumbnailStore = new JsonRest({ target: "api/index.php/photos" });
 
-			togGrid = new Toggler({
-				node: "gridContainer",
-			    showDuration: 1000,
-			    hideDuration: 1000
-			});
-			togPhoto = new Toggler({
-				node: "photoContainer",
-			    showDuration: 3000,
-			    hideDuration: 5000
-			});
-			
-			togmainGrid= new Toggler({
-				node: "mainGrid",
-			    showDuration: 3000,
-			    hideDuration: 5000
-			});
-			
         	d = domConstruct.create("div", { id: "mainGrid" });
         	domConstruct.place(d, "main");
 
         	divGrid  = domConstruct.create("div", { id: "gridContainer" });
         	divPhoto = domConstruct.create("div", { id: "photoContainer" });
         	domConstruct.place(divGrid, dom.byId("mainGrid"));
-	//		domConstruct.place(divPhoto, "mainGrid");
-
-        		
-		//	setTimeout(togmainGrid.hide(), 1000);
 			
-                photoHandle = topic.subscribe("havePhoto", function(data)  {
-                	fade("Out","gridContainer")
-                	setTimeout(function(){
-		               	domConstruct.empty(dom.byId("mainGrid"));
-		               	domConstruct.place(divPhoto, "mainGrid");
-	    	           	fade("In", "photoContainer" )
-	                	photoHandle.remove();
-                		
-                	}, 1000);
-					console.log("from app_gridTest  data :", data);					
-                });
-            	
-
-				// photo = new PhotoWidget({
-				//	 data: 1
-				// },"mainGrid"); 	
-				
-				grid = new GridWidget({
-					aStore: thumbnailStore
-				},"gridContainer"); 	
-		//		fade("In","gridContainer");
-            // }
-//     	    
+            photoHandle = topic.subscribe("havePhoto", function(data)  {
+            	fade("Out","gridContainer")
+            	setTimeout(function(){
+	               	domConstruct.empty(dom.byId("mainGrid"));
+	               	domConstruct.place(divPhoto, "mainGrid");
+					photo = new PhotoWidget({
+						 theData: data
+					},"photoContainer"); 	
+    	           	fade("In", "photoContainer" )
+                	photoHandle.remove();
+            		
+            	}, 1000);
+		//		console.log("from app_gridTest  data :", data);					
+            });
+            					
+			grid = new GridWidget({
+				aStore: thumbnailStore
+			},"gridContainer"); 	
 		},
 		fade = function(dir,node) {
 			if (dir === 'Out') { baseFx.fadeOut({ node: dom.byId(node), duration : 800 }).play(); }
--- js/custom/photoWidget/photoWidget.js
+++ js/custom/photoWidget/photoWidget.js
@@ -1,8 +1,8 @@
 define(["dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin", 
-		"dojo/text!./templates/photoWidget.html", "dojo/topic"
+		"dojo/text!./templates/photoWidget.html", "dojo/topic", "dojo/dom", "dojox/gfx",
 		],
     function(declare, WidgetBase, TemplatedMixin, 
-    		 template, topic
+    		 template, topic, dom, gfx
     		) {
         return declare([WidgetBase, TemplatedMixin], {
         	name: "No Name",
@@ -12,11 +12,36 @@ define(["dojo/_base/declare","dijit/_WidgetBase", "dijit/_TemplatedMixin",
             baseClass: "photoWidget",
  
    			postCreate: function(){
-
+				var fw, fh,
+                surfaceHandle = topic.subscribe("haveSurface", function(surface,theData)  {
+                	image = surface.createImage({ x: 10, y: 10, width: (theData.fx / 2), height: (theData.fy / 2), src: "img/" + theData.fname });
+                	surfaceHandle.remove();
+                //	paintMarkUpDiv(photo);
+                //	fade('In', "markupGridContainer");
+                	// baseFx.fadeIn({ node: dom.byId("markupGridContainer"), duration: 500 }).play();
+					
+                });
 				this.inherited(arguments);   // Run any parent postCreate processes - can be done at any point
-
-                        
+				console.log("theData :", this.theData);
+      	        fw = (this.theData.fx / 2) + 10;
+	            fh = (this.theData.fy / 2) + 25;
+	            
+	            createSurface(this.theData);                  
 			}
         });
+		function createSurface(data) {
+            var divSurfaceElement = dom.byId("photoNode");
+			if (!divSurfaceElement) {
+				debugger;
+			} else {
+	            surface = gfx.createSurface(divSurfaceElement, data.fx, data.fy);
+	
+	            surface.whenLoaded(function () {
+//	                console.log("surface loaded (tn)");
+	                topic.publish("haveSurface",surface, data);
+	            });
+				
+			}
+    	};        
 });
 
